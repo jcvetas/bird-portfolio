98d8f938428222c269f4c6991ccb1a35f4b97228
Abby Heath <abbyheath@Abbys-MacBook-Pro-2.local>
2021-05-25 20:01:03 -0500

    added content to homepage & added gallery + about page

---
+++ sidebar_rest.js
@@ -0,0 +1,20 @@
+var open = false;
+
+document.querySelector("main").onclick = (ev) => {
+    if (open && ev.target.className != "openbtn") {
+        closeAside();
+    }
+}
+
+function openAside() {
+    document.getElementById("sidebar").style.width = "300px";
+    document.querySelector(".openbtn").style.display = "none";
+    open = true;
+}
+
+function closeAside() {
+    document.querySelector("aside").style.width = "0";
+    //document.querySelector("main").style.marginRight = "0";
+    document.querySelector(".openbtn").style.display = "block";
+    side = false;
+}
